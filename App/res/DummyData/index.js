f0f7375a06c90238b54d87d2ba33a76894c26fde
Raj Shah <rajshah23471@gmail.com>
2021-04-17 10:13:07 +0530

    - Add Required Images

---
+++ App/res/DummyData/index.js
@@ -0,0 +1,35 @@
+import { images } from '../images';
+export default [
+    {
+        type: 'Humlan P',
+        imageUri: images.urbanears_blue,
+        heading: 'Vibrant colors',
+        description: 'Four on-trend colorways to seamlessly suit your style.',
+        key: 'first',
+        color: '#9dcdfa',
+    },
+    {
+        type: 'Pampas',
+        imageUri: images.urbanears_pink,
+        heading: 'Redefined sound',
+        description: 'A bold statement tuned to perfection.',
+        key: 'second',
+        color: '#db9efa',
+    },
+    {
+        type: 'Humlan P',
+        imageUri: images.urbanears_grey,
+        heading: 'Great quality',
+        description: 'An Urbanears classic! Listen-all-day fit. Striking the perfect balance of effortless technology',
+        key: 'third',
+        color: '#999',
+    },
+    {
+        type: 'Humlan B',
+        imageUri: images.urbanears_mint,
+        heading: 'From Sweden',
+        description: 'The “Plattan” in Plattan headphones is Swedish for “the slab.”',
+        key: 'fourth',
+        color: '#a1e3a1',
+    },
+];
---
+++ App/res/images.js
@@ -0,0 +1,7 @@
+export const images = {
+    ue_black_logo: require('./Images/ue_black_logo.png'),
+    urbanears_blue: require('./Images/urbanears_blue.png'),
+    urbanears_grey: require('./Images/urbanears_grey.png'),
+    urbanears_mint: require('./Images/urbanears_mint.png'),
+    urbanears_pink: require('./Images/urbanears_pink.png')
+};
